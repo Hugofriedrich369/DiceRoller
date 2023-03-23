f30cdefdec35e6bd1e9b3e06d1b42c8dcc0d5250
Hugofriedrich369 <hungos0223@gmail.com>
2023-03-22 22:32:13 -0300

    [DiceRoller] Projeto finalizado

--- app/src/main/java/com/ds2/diceroller/MainActivity.kt
+++ app/src/main/java/com/ds2/diceroller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.ds2.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import com.google.android.material.snackbar.Snackbar
@@ -14,24 +15,29 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val rollButton: Button = findViewById(R.id.button)
-
         rollButton.setOnClickListener {
             val toast = Toast.makeText(this, "rolou um dado!", Toast.LENGTH_SHORT).show()
-            val resultTextView: TextView = findViewById(R.id.textView)
-
-            class Dice (val numSides: Int){
-
+            class Dice (val numSides: Int) {
                 fun roll(): Int {
                     return (1..numSides).random()
                 }
-
             }
+            fun rollDice() {
+                val dice = Dice(6)
+                val diceRoll = dice.roll()
+                val diceImage: ImageView = findViewById(R.id.imageView5)
 
-            val dice = Dice(6)
-            val diceRoll = dice.roll()
-
-            resultTextView.text = diceRoll.toString()
+                val drawableResource = when (diceRoll) {
+                    1 -> R.drawable.dice_1
+                    2 -> R.drawable.dice_2
+                    3 -> R.drawable.dice_3
+                    4 -> R.drawable.dice_4
+                    5 -> R.drawable.dice_5
+                    else -> R.drawable.dice_6
+                }
+                diceImage.setImageResource(drawableResource)
+            }
+            rollDice()
         }
-
     }
 }
